ae7c760e13684409513a87c3e5242bc34b463b01
giorgiadp <dipietrogiorgia@libero.it>
2019-04-14 18:41:04 +0200

    clock stabile a 32

--- prova_clock/pcint.c
+++ prova_clock/pcint.c
@@ -4,20 +4,19 @@
 #include <avr/io.h>
 #include <avr/interrupt.h>
 
-#include "../avr_common/uart.h" // this includes the printf and initializes it
+#include "./avr_common/uart.h" // this includes the printf and initializes it
 
 #define PIN_MASK 0x80
 
 
-int in = 0;
+int att = 0, prec=0;;
 
 
 ISR(PCINT0_vect)
 {
-	if(in) in = 0;
-	else in = 1;
-
-	printf("%d\n", in);
+	att=TCNT1;
+	printf("%d\n", att-prec);
+	prec=att;
 }
 
 
@@ -25,14 +24,16 @@ ISR(PCINT0_vect)
 int main(void)
 {
 	printf_init();
+	TCCR1A=0;
+	TCNT1=0;
+	TCCR1B=(1<<CS10) | (1<<CS12);
 	DDRB &= ~PIN_MASK;
 	PORTB |= PIN_MASK;
 	PCICR |= (1 << PCIE0);
 	PCMSK0 |= PIN_MASK;
 	sei();
 
-	while(1)
-	{
+	while(1){
 		//printf("%d\n", PCMSK0);
 	}
 
--- prova_clock/start_clock.c
+++ prova_clock/start_clock.c
@@ -2,7 +2,7 @@
 #include <stdio.h>
 #include <stdint.h>
 #include <avr/io.h>
-#include "../avr_common/uart.h"	// this includes the printf and initializes it
+#include "./avr_common/uart.h"	// this includes the printf and initializes it
 
 
 //WGM setta il tipo di pwm che voglio (fast, phase correct, ctc...)
