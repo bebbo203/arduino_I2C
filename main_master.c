f47ae6e162b0d4bbd8ade4c2ef6ea1da60b21d17
Roberto Aureli <bebbo203@gmail.com>
2019-05-16 10:42:18 +0200

    master main

--- main_master.c
+++ main_master.c
@@ -5,26 +5,39 @@
 
 int main(void)
 {
-	printf_init();
 
-
+	/*printf_init();
 	master_init();
 
 	printf("START\n");
 	
 	Queue Q, *queue;
 	queue = &Q;
+	init_queue(queue);
 
 	master_request(queue, 0x33, 3);
 
 	for(int i=0;i<3;i++)
-		printf("%2X\n", dequeue(queue));
+		printf("%X\n", dequeue(queue));
 
 	
 
 	printf("END\n");
 	
-	while(1);
+	while(1);*/
+
+	printf_init();
+	master_init();
+
+	Queue Q, *queue;
+	queue = &Q;
+	init_queue(queue);
+
+	enqueue(queue, 0xAA);
+	enqueue(queue, 0xBB);
+	enqueue(queue, 0xCC);
+
+	master_send(0x33, queue, 3);
 	
 }
 
