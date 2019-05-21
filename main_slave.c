652dd26a7c643555d894ad6e3af459d48593b290
Giulai <giu.delci@gmail.com>
2019-05-21 13:15:42 +0200

    fine

--- main_slave.c
+++ main_slave.c
@@ -11,57 +11,25 @@
 int main(void)
 {
     printf_init();
-    //printf("bebbo fa schifo \n");
     slave_init(0x33);
+    
+	slave_sync(10);
 	
-	//PROVA SLAVE_RECEIVE
-	printf(" START\n");
-	Queue Q, *queue;
+	Queue Q, *queue, *q_clone, copia;
 	queue = &Q;
 	init_queue(queue);
-	
-	int i;
-	while(queue->size<10) {
-		slave_receive(queue);
-		printf("size: %d\n", queue->size);
-	}
-	while(queue->size>0) dequeue(queue);
-	printf("prontoooo\n");
+	q_clone = &copia;
 	
 	while(1){
-		//printf("prima\n");
 		slave_receive(queue);
-		//printf("sec\n");
 		
-		//printf("--->FINITO REC-> dim : %d\n", queue->size)
-		while(queue->size>0){
-			printf("%c", dequeue(queue));
-		}
-		printf(" FINE\n");
-	}
-	
-	
-	//PROVA SLAVE_SEND
-	/*
-	struct Queue Q, *queue;
-	queue = &Q;
-	
-	int i;
-	printf("inizio\n");
-	for(i=0; i<20;i++){
-		init_queue(queue);
-		enqueue(queue, 0xAA);
-		enqueue(queue, 0xAB);
-		enqueue(queue, 0xAC);
+		clone(queue, q);
 		
-		slave_send(queue, 3); 
-		//printf("e: %d\n", i);
+		slave_send(queue, queue->size);
+		
+		printf("Ho ricevuto: ");
+		while(q->size>0) printf("%c", dequeue(q));
+
 	}
-	
-	printf("fine\n");*/
-	//while(1);
-	
-
-
 
 }
