ee52f5b67be9f77ebb1c2386a2d19ce898c6e356
Roberto Aureli <bebbo203@gmail.com>
2019-05-21 13:21:42 +0200

    ritocco

--- main_master.c
+++ main_master.c
@@ -9,100 +9,38 @@ int main(void)
 	printf_init();
 	master_init();
 
-/*	printf("START\n");
-	
-	Queue Q, *queue;
-	queue = &Q;
-	init_queue(queue);
-
-	int i;
-	for(i=0; i<21; i++){ //anche qui mando un pacchetto in più (vedi sotto)
-						 //NB IL MASTER VA SEMPRE RIPULITO!
-		master_request(queue, 0x33, 3);
-		//printf(">>>%d\n", i);
-	}
-	//printf("Size: %d\n", queue->size);
-	dequeue(queue);
-	dequeue(queue);
-	dequeue(queue);
-	for(i=0;i<60;i++)
-		printf("%X\n", dequeue(queue));
 
 	
-
-	printf("END\n");
-	
-	while(1);
-*/
-
-//MASTER_SEND NO SERIALE
-/*
-	//printf_init();
-//	master_init();
-
-	Queue Q, *queue;
-	queue = &Q;
-
-	int i;
-	for(i=0; i<3; i++){ //mando sempre un pacchetto in più che mi garantisce la pulizia della seriale
-						 //lo slave comunque riceve tutto quello che deve (ossia condizione-1)
-						 //in questo modo non ho bisogno di ricompilare tutto ogni volta che ricomincia 
-						 //la comunicazione, mi basta fare open/close su cutecom
-		init_queue(queue);
-		enqueue(queue, 0xAA);
-		enqueue(queue, 0xBB);
-		enqueue(queue, 0xCC);
-		master_send(0x33, queue, 3);
-	}
-
-
-	
-	while(1);
-*/
-
-	
-//MASTER_SEND SERIALE
-	//while(1)
-	//{
-		//printf("sono nel while\n");
-
-
-	Queue queue, dummy_queue;
+	Queue queue;
 	Queue* q = &queue;
-	Queue* d_q = &dummy_queue;
 
-	init_queue(d_q);
-	
-	for(int i=0; i<10; i++)
-		enqueue(d_q, 0x00);
+	printf("Attesa sincronizzazione");
+	master_sync(0x33, 10);
 
-    while(d_q->size > 0)
-		master_send(0x33, d_q, 1);
-
-	
+	char length;
 	while(1)
 	{
 		init_queue(q);
 	
-
-		printf("parla: \n");
+		printf("Inserisci un messaggio: \n");
 		
 		cli();
 		read_string(q);
 		sei();
-		
-		printf("ho letto %d\n", q->size);
 
-		master_send(0x33, q, q->size);
+		length = q->size;
+	   
+		printf("Invio del messaggio in corso...\n");
+		master_send(0x33, q, length);
 
+		printf("Richiesta dati in corso...\n");
+		master_request(q, 0x33, length);
+
+		printf("Messaggio ricevuto: ");
+		while(q->size > 0)
+			printf("%c", dequeue(q));
+   
 	}
-
-
-	//}
-	
-
-	
-
 
 }
 
